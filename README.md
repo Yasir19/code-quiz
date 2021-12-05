b1e575f2607961a2dde024e90fb953d78b6968a8
yasir19 <yasir.habboo@gmail.com>
2021-12-05 00:40:10 -0600

    qestion function

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # code-quiz
+## user story
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+## Acceptance Criteria
+GIVEN I am taking a code quiz
+## must have feature 
+1-WHEN I click the start button
+THEN a timer starts and I am presented with a question
+2- WHEN I answer a question
+THEN I am presented with another question
+3- WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+4-WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+5- WHEN the game is over
+THEN I can save my initials and score
+
+
+1.create a foor loop based on the question array 
+we dont need ul
+2-var answerBtn = document.createElement(btn, )
+3- create another for loop to loop through the answer
+
+
+    //get cuurent question object from the array 
+    var currentQuestion = questions[questionIndex];
+    questionEl.textContent=currentQuestion.question;
+    var questionIndex= 0;
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+var currentchoicesIndex =0;
 var questions =[
     {
      question:"Commnly used data types Do NOT include",
@@ -26,21 +26,54 @@ var questions =[
         answer:"console.log"
     }
 ];
-var time = questions.length * 20;
+
 
 
 
 //veriable to refrence DOM element 
-// var questionsEl =docoment.getElementById("questions");
+var questionEl =document.getElementById("questions-list");
+var selectionEl =document.getElementById("selection");
 var timerEl =document.getElementById("time");
 var startBtn =document.getElementById("start");
 var inputEl= document.getElementById("init");
 var submitBtn =document.getElementById("submit");
 
+
+
 //start the quiz
 var startQuiz = function(){
     // hide the start-quize div
     var startQuizEl =document.getElementById("start-quiz");
-    startQuizEl.setAttribute("calss", "hide");
+    startQuizEl.setAttribute("class", "hide");
+
+    //revel quastion
+    questionEl.style.display ="block";
+    //set timer
+    //1- define timer veriable and returen the question lenghth array * 15 to get 75 second 
+    //2- define a variable with Setinterval method that hvae function as unarrgument 
+    //3- reduce the timer by one 
+    //4- print the timer to the timer element by using textContent method
+    var timer = questions.length * 15; 
+  var time = setInterval(function(){
+    timer--;
+    timerEl.textContent=timer;
+  },1000);
+getQuestion();
 }
-startBtn.addEventListener("onclick",startQuiz);
+// get question function 
+var getQuestion = function(){
+
+    // loop over the question array 
+    for (var i=0;i< questions.length; i++){
+        questionEl.textContent=questions[i].question;
+    }
+         //create new button for each choise
+         var choicesBtn = document.createElement("button");
+         // choicesBtn.setAttribute("value",choice);
+         choicesBtn.innerHTML=questions[i]+ questions.choices;
+         //create li element
+         var listItem= document.createElement("li");
+         selectionEl.appendChild(listItem);
+         listItem.appendChild(choicesBtn);
+}
+startBtn.addEventListener("click",startQuiz);
